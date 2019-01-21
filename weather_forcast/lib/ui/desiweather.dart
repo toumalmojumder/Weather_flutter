67c47fd7fc190ab3c30168dbc0fa77256bf11833
Toumal Mojumder <toumalmojumder@gmail.com>
2019-01-22 03:14:49 +0600

    input city name get data from json

--- weather_forcast/lib/ui/desiweather.dart
+++ weather_forcast/lib/ui/desiweather.dart
@@ -5,17 +5,34 @@ import 'dart:convert';
 import '../util/utils.dart' as util;
 import 'package:http/http.dart' as http;
 
-
 class desiweather extends StatefulWidget {
   @override
   _desiweatherState createState() => _desiweatherState();
 }
 
 class _desiweatherState extends State<desiweather> {
+  String _cityEntered;
 
-  void showStuff() async{
+  Future _goToNextScreen(BuildContext context) async {
+    Map results = await Navigator
+        .of(context)
+        .push(new MaterialPageRoute<Map>(builder: (BuildContext context) { //change to Map instead of dynamic for this to work
+      return new ChangeCity();
+    }));
+
+    if ( results != null && results.containsKey('enter')) {
+      _cityEntered = results['enter'];
+
+//      debugPrint("From First screen" + results['enter'].toString());
+
+
+    }
+  }
+
+  void showStuff() async {
     getWeather(util.apiId, util.defaultCity);
-}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,46 +41,48 @@ class _desiweatherState extends State<desiweather> {
         centerTitle: true,
         backgroundColor: Colors.red,
         actions: <Widget>[
-
-          new IconButton(icon: new Icon(Icons.menu,color: Colors.white,), onPressed: null)
+          new IconButton(
+              icon: new Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                _goToNextScreen(context);
+              })
         ],
       ),
-      body:
-      new Stack(
+      body: new Stack(
         children: <Widget>[
           new Center(
               child: new Image(
                   width: 500.0,
                   height: 1200.0,
                   fit: BoxFit.cover,
-                  image: new AssetImage('images/umbrella.png')
-              )
-          ),
+                  image: new AssetImage('images/umbrella.png'))),
           new Container(
             alignment: Alignment.topRight,
-            margin: EdgeInsets.fromLTRB(0.0, 20, 20, 0.0),
-            child: new Text('Something',style: cityStyle())
-          ),
+            margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            child: new Text(
+              '${_cityEntered == null ? util.defaultCity : _cityEntered}',
+              style: cityStyle(),
+            ),),
           new Container(
               alignment: Alignment.center,
-              child: new Image.asset('images/light_rain.png')
-          ),
-          new Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.fromLTRB(0.0, 150, 50.0, 0.0),
-              child: new Text('97.5'+'F',style: tempStyle())
-          ),
+              child: new Image.asset('images/light_rain.png')),
+          updateTempWidget(_cityEntered)
+
         ],
       ),
-
     );
   }
-  Future<Map> getWeather(String apiId,String city ) async{
-    String apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=$city&APPID=$apiId';
+
+  Future<Map> getWeather(String apiId, String city) async {
+    String apiUrl =
+        'http://api.openweathermap.org/data/2.5/weather?q=$city&APPID=$apiId&units=metric';
     http.Response response = await http.get(apiUrl);
     return json.decode(response.body);
+  }
 
-}
   Widget updateTempWidget(String city) {
     return new FutureBuilder(
         future: getWeather(util.apiId, city == null ? util.defaultCity : city),
@@ -78,7 +97,7 @@ class _desiweatherState extends State<desiweather> {
                 children: <Widget>[
                   new ListTile(
                     title: new Text(
-                      content['main']['temp'].toString() +" F",
+                      content['main']['temp'].toString() + " C",
                       style: new TextStyle(
                           fontStyle: FontStyle.normal,
                           fontSize: 49.9,
@@ -87,12 +106,19 @@ class _desiweatherState extends State<desiweather> {
                     ),
                     subtitle: new ListTile(
                       title: new Text(
-                        "Humidity: ${content['main']['humidity'].toString()}\n"
-                            "Min: ${content['main']['temp_min'].toString()} F\n"
-                            "Max: ${content['main']['temp_max'].toString()} F ",
+                            "Humidity: ${content['main']['humidity'].toString() }%\n"
+                            "Pressure: ${content['main']['pressure'].toString() }hPa\n"
+                            "Min: ${content['main']['temp_min'].toString()} C\n"
+                            "Max: ${content['main']['temp_max'].toString()} C\n"
+                            "Visibility: ${content['visibility'].toString()} meter\n"
+                            "Sky: ${content['weather'][0]['description'].toString()}\n"
+                            "Speed: ${content['wind']['speed'].toString()}meter/sec\n"
+                                "Direction: ${content['wind']['deg'].toString()}degrees\n"
+                                "Rain: ${content['rain'].toString()}\n"
 
+
+                        ,
                         style: extraData(),
-
                       ),
                     ),
                   )
@@ -105,29 +131,74 @@ class _desiweatherState extends State<desiweather> {
         });
   }
 }
+class ChangeCity extends StatelessWidget {
 
-TextStyle cityStyle(){
-return new TextStyle(
-  color: Colors.white,
-  fontSize: 22.9,
-  fontStyle: FontStyle.italic
+  var _cityFieldController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: new Text('Change City'),
+        centerTitle: true,
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset(
+              'images/white_snow.png',
+              width: 490.0,
+              height: 1200.0,
+              fit: BoxFit.fill,
+            ),
+          ),
 
-);
+          new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new TextField(
+                  decoration: new InputDecoration(
+                    hintText: 'Enter City',
+                  ),
+                  controller: _cityFieldController,
+                  keyboardType: TextInputType.text,
+                ),
+
+              ),
+              new ListTile(
+                title: new FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context, {
+                        'enter': _cityFieldController.text
+                      });
+                    },
+                    textColor: Colors.white70,
+                    color: Colors.redAccent,
+                    child: new Text('Get Weather')),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
-TextStyle tempStyle(){
+
+TextStyle cityStyle() {
   return new TextStyle(
-      color: Colors.white,
-      fontSize: 49.9,
-      fontStyle: FontStyle.normal,
+      color: Colors.white, fontSize: 22.9, fontStyle: FontStyle.italic);
+}
+
+TextStyle tempStyle() {
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 49.9,
+    fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w500,
-
-
   );
 }
+
 TextStyle extraData() {
   return new TextStyle(
-      color: Colors.white70,
-      fontStyle: FontStyle.normal,
-      fontSize: 17.0);
-
+      color: Colors.white70, fontStyle: FontStyle.normal, fontSize: 17.0);
 }
--- weather_forcast/lib/util/utils.dart
+++ weather_forcast/lib/util/utils.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 final apiId ="acbff94a4bda7d9d46eca93511bdc59a";
-final defaultCity = "Dhaka";
+final defaultCity = "Dhaka,bd";
